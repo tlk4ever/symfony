96dd630f5147dc3567e7d86f47bba4b8d29a8d04
Thiago Brito <tbffreitas@gmail.com>
2017-02-27 18:38:22 -0300

    Criando classe Author e fazendo relacionamento com Post

--- src/Tableless/ModelBundle/Form/AuthorType.php
+++ src/Tableless/ModelBundle/Form/AuthorType.php
@@ -6,34 +6,34 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class PostType extends AbstractType {
-
+class AuthorType extends AbstractType
+{
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options) {
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
         $builder
-                ->add('title')
-                ->add('content')
-                ->add('author')
+            ->add('name')
         ;
     }
-
+    
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
         $resolver->setDefaults(array(
-            'data_class' => 'Tableless\ModelBundle\Entity\Post'
+            'data_class' => 'Tableless\ModelBundle\Entity\Author'
         ));
     }
 
     /**
      * @return string
      */
-    public function getName() {
-        return 'tableless_modelbundle_post';
+    public function getName()
+    {
+        return 'tableless_modelbundle_author';
     }
-
 }
