38c9c2bc1dd856b6861f9730cff3d8152a8e0afd
Thiago Brito <tbffreitas@gmail.com>
2017-04-08 11:30:21 -0300

    Parte 8 paginacao

--- src/Tableless/CoreBundle/Controller/IndexControllerController.php
+++ src/Tableless/CoreBundle/Controller/IndexControllerController.php
@@ -5,6 +5,7 @@ namespace Tableless\CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 
 class IndexControllerController extends Controller {
 
@@ -12,11 +13,15 @@ class IndexControllerController extends Controller {
      * @Route("/",name="index")
      * @Template()
      */
-    public function indexAction() {
+    public function indexAction(Request $request) {
         $em = $this->getDoctrine()->getManager();
         $posts = $em->getRepository('TablelessModelBundle:Post')->findAllInOrder();
+        
+         /** @var  $paginator */
+        $paginator  = $this->get('knp_paginator');
+        $pagination = $paginator->paginate($posts, $request->query->get('page', 1), 3);
         return array(
-            'posts' => $posts,
+            'pagination' => $pagination,
         );
     }
 
